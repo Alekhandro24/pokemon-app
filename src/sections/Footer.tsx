ad6bf06419026bcd48d3b73f1d0ad960da75bf43
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-01 17:29:48 +0300

    Create Footer.tsx

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { MdOutlinePowerSettingsNew } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="block"></div>
+      <div className="data"></div>
+      <div className="block">
+        <MdOutlinePowerSettingsNew />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
