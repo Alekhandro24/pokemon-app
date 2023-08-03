93dd656582b8a5e3c834b7d1ab0705f5e27cb4df
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-03 17:11:11 +0300

    Create getPokemonImages.ts

---
+++ src/utils/getPokemonImages.ts
@@ -0,0 +1,27 @@
+//@ts-nocheck
+const fetchImages = (context: string) => {
+  const images = {};
+  const cache = {};
+
+  function importAll(r) {
+    r.keys().forEach((key) => (cache[key] = r(key)));
+  }
+
+  importAll(context);
+
+  Object.entries(cache).forEach((module: string[]) => {
+    let key = module[0].split("");
+    key.splice(0, 2);
+    key.splice(-4, 4);
+    images[[key.join("")]] = module[1];
+  });
+  return images;
+};
+
+export const images = fetchImages(
+  require.context("../assets/shiny", false, /\.(png|jpe?g|svg)$/)
+);
+
+export const defaultImages = fetchImages(
+  require.context("../assets/default", false, /\.(png|jpe?g|svg)$/)
+);
