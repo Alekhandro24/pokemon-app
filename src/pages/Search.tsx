bb956e94105d053f026fc8ade3e498e9f5c760fd
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-03 17:10:47 +0300

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Wrapper from "../sections/Wrapper";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getInitialPokemonData } from "../app/reducers/getInitialPokemonData";
+import { getPokemonData } from "../app/reducers/getPokemonData";
 
 const Search = () => {
+  const dispatch = useAppDispatch();
+  const { allPokemon } = useAppSelector(({ pokemon }) => pokemon);
+
+  useEffect(() => {
+    dispatch(getInitialPokemonData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (allPokemon) {
+      const clonedPokemons = [...allPokemon];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonData(randomPokemonsId));
+    }
+  }, [allPokemon, dispatch]);
+
   return <div>Search</div>;
 };
 
-export default Search;
+export default Wrapper(Search);
