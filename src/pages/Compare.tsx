ca2513d0efdee1d9a8365beac1a351de0d9230d6
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-04 21:24:46 +0300

    Update Compare.tsx

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
+import { useAppSelector } from "../app/hooks";
+import CompareContainer from "../components/CompareContainer/CompareContainer";
+
 const Compare = () => {
-  return <div>Compare</div>;
+  const { compareQueue } = useAppSelector(({ pokemon }) => pokemon);
+
+  return (
+    <div className="compare">
+      <CompareContainer
+        pokemon={compareQueue[0]}
+        isEmpty={compareQueue.length < 1}
+      />
+
+      <CompareContainer
+        pokemon={compareQueue[1]}
+        isEmpty={compareQueue.length < 2}
+      />
+    </div>
+  );
 };
 
 export default Wrapper(Compare);
