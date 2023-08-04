be8574536d6d96625be734d97cd42c07eda579e5
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-04 21:24:42 +0300

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,10 +3,14 @@ import Wrapper from "../sections/Wrapper";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { getInitialPokemonData } from "../app/reducers/getInitialPokemonData";
 import { getPokemonData } from "../app/reducers/getPokemonData";
+import PokemonCardGrid from "../components/PokemonCardGrid/PokemonCardGrid";
+import { debounce } from "../utils/Debounce";
 
 const Search = () => {
   const dispatch = useAppDispatch();
-  const { allPokemon } = useAppSelector(({ pokemon }) => pokemon);
+  const { allPokemon, randomPokemons } = useAppSelector(
+    ({ pokemon }) => pokemon
+  );
 
   useEffect(() => {
     dispatch(getInitialPokemonData());
@@ -22,7 +26,37 @@ const Search = () => {
     }
   }, [allPokemon, dispatch]);
 
-  return <div>Search</div>;
+  const handleChange = debounce((value: string) => getPokemon(value), 1000);
+
+  const getPokemon = async (value: string) => {
+    if (value.length) {
+      const pokemons = allPokemon?.filter((pokemon) =>
+        pokemon.name.includes(value.toLowerCase())
+      );
+      dispatch(getPokemonData(pokemons!));
+    } else {
+      const clonedPokemons = [...(allPokemon as [])];
+      const randomPokemonsId = clonedPokemons
+        .sort(() => Math.random() - Math.random())
+        .slice(0, 20);
+      dispatch(getPokemonData(randomPokemonsId));
+    }
+  };
+
+  return (
+    <>
+      <div className="search">
+        <input
+          type="text"
+          className="pokemon-searchbar"
+          placeholder="Search Pokemon"
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <PokemonCardGrid pokemons={randomPokemons!} />
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Wrapper(Search);
