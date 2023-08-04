e693671a76f7516a4ff146ae8b2208ba76fe8e84
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-04 21:24:39 +0300

    Create PokemonCardGrid.tsx

---
+++ src/components/PokemonCardGrid/PokemonCardGrid.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { pokemonTypeInterfase, userPokemonsType } from "../../utils/Types";
+import { IoGitCompare } from "react-icons/io5";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hooks";
+import { addToCompare } from "../../app/slices/PokemonSlice";
+import { setToast } from "../../app/slices/AppSlice";
+
+const PokemonCardGrid = ({ pokemons }: { pokemons: userPokemonsType[] }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="pokemon-card-grid-container">
+      <div className="pokemon-card-grid">
+        {pokemons &&
+          pokemons.length > 0 &&
+          pokemons?.map((data: userPokemonsType) => {
+            return (
+              <div className="pokemon-card" key={data.id}>
+                <div className="pokemon-card-list">
+                  {location.pathname.includes("/pokemon") ||
+                  location.pathname.includes("/search") ? (
+                    <FaPlus className="plus" />
+                  ) : (
+                    <FaTrash className="trash" />
+                  )}
+                </div>
+                <div className="pokemon-card-compare">
+                  <IoGitCompare
+                    onClick={() => {
+                      dispatch(addToCompare(data));
+                      dispatch(
+                        setToast(`${data.name} has been added to Compare Queue`)
+                      );
+                    }}
+                  />
+                </div>
+                <h3 className="pokemon-card-title">{data.name}</h3>
+                <img
+                  src={data.image}
+                  alt="pokemon img"
+                  className="pokemon-card-image"
+                  loading="lazy"
+                  onClick={() => navigate(`/pokemon/${data.id}`)}
+                />
+
+                <div className="pokemon-card-types">
+                  {data.types.map(
+                    (type: pokemonTypeInterfase, index: number) => {
+                      const keys = Object.keys(type);
+
+                      return (
+                        <div className="pokemon-card-types-type" key={index}>
+                          <img
+                            src={type[keys[0]].image}
+                            alt="pokemon pic"
+                            className="pokemon-card-types-type-image"
+                            loading="lazy"
+                          />
+                          <h6 className="pokemon-card-types-type-text">
+                            {keys[0]}
+                          </h6>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCardGrid;
