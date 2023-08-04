93ec2d44d0660542bab0d11628fc08bd5b31db4b
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-04 21:24:10 +0300

    Update PokemonSlice.ts

--- src/app/slices/PokemonSlice.ts
+++ src/app/slices/PokemonSlice.ts
@@ -1,20 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { PokemonTypeInitialState } from "../../utils/Types";
+import { createSlice, isAction } from "@reduxjs/toolkit";
+import {
+  PokemonTypeInitialState,
+  generatedPokemonType,
+} from "../../utils/Types";
 import { getInitialPokemonData } from "../reducers/getInitialPokemonData";
+import { getPokemonData } from "../reducers/getPokemonData";
 
 const initialState: PokemonTypeInitialState = {
   allPokemon: undefined,
+  randomPokemons: undefined,
+  compareQueue: [],
 };
 
 export const PokemonSlice = createSlice({
   name: "pokemon",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCompare: (state, action) => {
+      const index = state.compareQueue.findIndex(
+        (pokemon: generatedPokemonType) => pokemon.id === action.payload.id
+      );
+
+      if (index === -1) {
+        if (state.compareQueue.length === 2) {
+          state.compareQueue.pop();
+        }
+        state.compareQueue.unshift(action.payload);
+      }
+    },
+    removeFromCompare: (state, action) => {
+      const index = state.compareQueue.findIndex(
+        (pokemon: generatedPokemonType) => pokemon.id === action.payload.id
+      );
+      const queue = [...state.compareQueue];
+      queue.splice(index, 1);
+      state.compareQueue = queue;
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(getInitialPokemonData.fulfilled, (state, action) => {
-      state.allPokemon = action.payload;
-    });
+    builder
+      .addCase(getInitialPokemonData.fulfilled, (state, action) => {
+        state.allPokemon = action.payload;
+      })
+      .addCase(getPokemonData.fulfilled, (state, action) => {
+        state.randomPokemons = action.payload;
+      });
   },
 });
 
-export const {} = PokemonSlice.actions;
+export const { addToCompare, removeFromCompare } = PokemonSlice.actions;
