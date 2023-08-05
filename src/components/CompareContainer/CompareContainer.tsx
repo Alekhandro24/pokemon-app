32fb01e0cc9bc0091bd0cf40bf41f2ea4d54932a
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-05 12:32:26 +0300

    Update CompareContainer.tsx

--- src/components/CompareContainer/CompareContainer.tsx
+++ src/components/CompareContainer/CompareContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { userPokemonsType } from "../../utils/Types";
+import { pokemonTypeInterfase, userPokemonsType } from "../../utils/Types";
+import { FaPlus } from "react-icons/fa";
 
 const CompareContainer = ({
   pokemon = undefined,
@@ -8,11 +9,55 @@ const CompareContainer = ({
   pokemon?: userPokemonsType;
   isEmpty?: boolean;
 }) => {
+  const getStats = () => {};
+
   return (
     <div className="compare-container">
       {isEmpty && (
         <div className="empty">
-          <button></button>
+          <button>
+            <FaPlus />
+          </button>
+          <h3>Add Pokemon to Comparison</h3>
+        </div>
+      )}
+      {pokemon && (
+        <div className="compare-element">
+          <div className="compare-info">
+            <div className="compare-details">
+              <h3>{pokemon?.name}</h3>
+              <img
+                src={pokemon?.image}
+                alt="pokemon"
+                className="compare-image"
+              />
+            </div>
+            <div className="pokemon-types-container">
+              <div className="pokemon-types">
+                <h4 className="pokemon-type-title">Type</h4>
+                <div className="pokemon-type-icons">
+                  {pokemon?.types.map((type: pokemonTypeInterfase) => {
+                    const keys = Object.keys(type);
+                    return (
+                      <div className="pokemon-type">
+                        <img
+                          src={type[keys[0]].image}
+                          alt="pokemon type"
+                          className="pokemon-type-image"
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              {/* {getStats} */}
+            </div>
+          </div>
+          <div className="compare-action-buttons">
+            <button className="compare-btn">Add</button>
+            <button className="compare-btn">View</button>
+            <button className="compare-btn">Remove</button>
+          </div>
         </div>
       )}
     </div>
