796e20bd0d03ef45205e0400b3ec5b69e9ffc51e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-06 16:08:34 +0300

    Create Login.tsx

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,40 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { addDoc, getDocs, query, where } from "firebase/firestore";
+import { firebaseAauth, usersRef } from "../../utils/FirebaseCinfig";
+import { useAppDispatch } from "../../app/hooks";
+import { setUserStatus } from "../../app/slices/AppSlice";
+
+const Login = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const {
+      user: { email, uid },
+    } = await signInWithPopup(firebaseAauth, provider);
+
+    if (email) {
+      const firestoreQuery = query(usersRef, where("uid", "==", uid));
+      const fetchUser = await getDocs(firestoreQuery);
+
+      if (fetchUser.docs.length === 0) {
+        await addDoc(usersRef, { uid, email });
+      }
+
+      dispatch(setUserStatus({ email }));
+    }
+  };
+
+  return (
+    <div className="login">
+      <button className="login-btn" onClick={handleLogin}>
+        <FcGoogle />
+        Login with Google
+      </button>
+    </div>
+  );
+};
+
+export default Login;
