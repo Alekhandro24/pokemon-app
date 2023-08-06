c88ad25670130bf2b0f5a1d2f1069ed70d74c2ef
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-06 16:08:42 +0300

    Update MyList.tsx

--- src/pages/MyList.tsx
+++ src/pages/MyList.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
+import { useAppSelector } from "../app/hooks";
+import Login from "../components/Login/Login";
+
 const MyList = () => {
-  return <div>MyList</div>;
+  const { userInfo } = useAppSelector(({ app }) => app);
+
+  return (
+    <div className="list">
+      <Login />
+    </div>
+  );
 };
 
 export default Wrapper(MyList);
