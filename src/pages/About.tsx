e292bc295324c343d1c75295efd20e389c76a807
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-08 16:35:45 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,29 @@
-import React from "react";
 import Wrapper from "../sections/Wrapper";
+import avatarImage from "../assets/pikachu.png";
+import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="profile">
+      <img src={avatarImage} alt="avatarImage" className="profile-image" />
+      <h1 className="profile-text">Hi I'm Pikachu</h1>
+      <h2 className="profile-text">The creator of this awesome pokedex</h2>
+      <h4 className="profile-text">
+        This pet-project is create for my portfolio
+      </h4>
+      <div className="profile-links">
+        <a href="/">
+          <FaGithub />
+        </a>
+        <a href="/">
+          <FaYoutube />
+        </a>
+        <a href="/">
+          <FaLinkedin />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Wrapper(About);
