6b5e9b78ba2e28500067e5a72862320573ebabab
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-08 16:35:43 +0300

    Update PokemonCardGrid.tsx

--- src/components/PokemonCardGrid/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid/PokemonCardGrid.tsx
@@ -6,6 +6,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../app/hooks";
 import { addToCompare } from "../../app/slices/PokemonSlice";
 import { setToast } from "../../app/slices/AppSlice";
+import { addPokemonToList } from "../../app/reducers/addPokemonToList";
+import { removePokemon } from "../../app/reducers/removePokemonFromUserList";
 
 const PokemonCardGrid = ({ pokemons }: { pokemons: userPokemonsType[] }) => {
   const location = useLocation();
@@ -23,9 +25,18 @@ const PokemonCardGrid = ({ pokemons }: { pokemons: userPokemonsType[] }) => {
                 <div className="pokemon-card-list">
                   {location.pathname.includes("/pokemon") ||
                   location.pathname.includes("/search") ? (
-                    <FaPlus className="plus" />
+                    <FaPlus
+                      className="plus"
+                      onClick={() => dispatch(addPokemonToList(data))}
+                    />
                   ) : (
-                    <FaTrash className="trash" />
+                    <FaTrash
+                      className="trash"
+                      onClick={async () => {
+                        await dispatch(removePokemon({ id: data.firebaseId! }));
+                        dispatch(setToast("Pokemon removed successfully"));
+                      }}
+                    />
                   )}
                 </div>
                 <div className="pokemon-card-compare">
