afa222b69b56918fa2c45677be8f8f73406efbfb
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-08 16:35:49 +0300

    Update MyList.tsx

--- src/pages/MyList.tsx
+++ src/pages/MyList.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../sections/Wrapper";
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import Login from "../components/Login/Login";
+import PokemonCardGrid from "../components/PokemonCardGrid/PokemonCardGrid";
+import { getUserPokemons } from "../app/reducers/getUserPokemons";
 
 const MyList = () => {
   const { userInfo } = useAppSelector(({ app }) => app);
+  const { userPokemons } = useAppSelector(({ pokemon }) => pokemon);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getUserPokemons());
+  }, [userInfo, dispatch]);
+
+  useEffect(() => {
+    dispatch(getUserPokemons());
+  }, [userInfo, dispatch]);
 
   return (
     <div className="list">
-      <Login />
+      {userInfo ? <PokemonCardGrid pokemons={userPokemons} /> : <Login />}
     </div>
   );
 };
