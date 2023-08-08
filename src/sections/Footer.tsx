6b04513645a6d98201f0a1dcf2ea664c5563d1ae
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-08 16:35:59 +0300

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { signOut } from "firebase/auth";
 import { MdOutlinePowerSettingsNew } from "react-icons/md";
+import { firebaseAuth } from "../utils/FirebaseCinfig";
+import { useAppDispatch } from "../app/hooks";
+import { setToast, setUserStatus } from "../app/slices/AppSlice";
 
 const Footer = () => {
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    signOut(firebaseAuth);
+    dispatch(setUserStatus(undefined));
+    dispatch(setToast("Logged out succesfully from Firebase. "));
+  };
+
   return (
     <footer>
       <div className="block"></div>
       <div className="data"></div>
       <div className="block">
-        <MdOutlinePowerSettingsNew />
+        <MdOutlinePowerSettingsNew onClick={handleLogout} />
       </div>
     </footer>
   );
