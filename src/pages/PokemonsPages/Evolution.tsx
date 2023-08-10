69e2351bd7d48dc28760a60ae0d225dc718b109f
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-10 12:12:48 +0300

    Update Evolution.tsx

--- src/pages/PokemonsPages/Evolution.tsx
+++ src/pages/PokemonsPages/Evolution.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { getPokemonData } from "../../app/reducers/getPokemonData";
+import PokemonCardGrid from "../../components/PokemonCardGrid/PokemonCardGrid";
 
 const Evolution = () => {
-  return <div>Evolution</div>;
+  const dispatch = useAppDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { currentPokemon, randomPokemons } = useAppSelector(
+    ({ pokemon }) => pokemon
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pokemons = currentPokemon?.evolution.map(({ pokemon }) => pokemon);
+
+      await dispatch(getPokemonData(pokemons!));
+      setIsLoaded(true);
+    };
+    fetchData();
+  }, [dispatch, currentPokemon]);
+
+  return (
+    <div className="page">
+      {isLoaded && <PokemonCardGrid pokemons={randomPokemons!} />}
+    </div>
+  );
 };
 
 export default Evolution;
