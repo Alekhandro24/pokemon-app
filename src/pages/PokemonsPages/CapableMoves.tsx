e94107b3d7742ac5c493bd779a984cf3ce066184
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-10 12:12:44 +0300

    Update CapableMoves.tsx

--- src/pages/PokemonsPages/CapableMoves.tsx
+++ src/pages/PokemonsPages/CapableMoves.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useAppSelector } from "../../app/hooks";
 
 const CapableMoves = () => {
-  return <div>CapableMoves</div>;
+  const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+  );
+
+  return (
+    <div className="capable-moves">
+      <h1 className="capable-moves-title">Abilities</h1>
+      <ul className="capable-moves-list ability">
+        {pokemonData?.pokemonAbilities.abilities.map((ability: string) => (
+          <li key={ability} className="move">
+            {ability}
+          </li>
+        ))}
+      </ul>
+
+      <h1 className="capable-moves-title">Moves</h1>
+      <ul className="capable-moves-list">
+        {pokemonData?.pokemonAbilities.moves.map((move: string) => (
+          <li key={move} className="move">
+            {move}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CapableMoves;
