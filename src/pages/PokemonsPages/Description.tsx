8bccaa7bb315b03e2ca8da01a978fc278c0f4d0c
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-08-10 12:12:46 +0300

    Update Description.tsx

--- src/pages/PokemonsPages/Description.tsx
+++ src/pages/PokemonsPages/Description.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useAppSelector } from "../../app/hooks";
+import PokemonContainer from "../../components/PokemonContainer/PokemonContainer";
+import Info from "../../components/Info/Info";
 
 const Description = () => {
-  return <div>Description</div>;
+  const pokemonData = useAppSelector(
+    ({ pokemon: { currentPokemon } }) => currentPokemon
+  );
+  return (
+    <>
+      {pokemonData && (
+        <>
+          <Info data={pokemonData} />
+          <PokemonContainer image={pokemonData?.image!} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Description;
